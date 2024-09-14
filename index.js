a5dd3f28ffd56d58ff4488f37d545daea389a6b1
thiagotkd2 <thiagop.pf.moraes@gmail.com>
2024-09-14 12:47:08 -0300

    Commit 1 - Extração de Função

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ function gerarFaturaStr (fatura, pecas) {
                           { style: "currency", currency: "BRL",
                             minimumFractionDigits: 2 }).format;
   
-    for (let apre of fatura.apresentacoes) {
-      const peca = pecas[apre.id];
-      let total = 0;
+      function calcularTotalApresentacao(apre,peca){
+        let total = 0;
   
       switch (peca.tipo) {
       case "tragedia":
@@ -29,7 +28,12 @@ function gerarFaturaStr (fatura, pecas) {
       default:
           throw new Error(`Peça desconhecia: ${peca.tipo}`);
       }
-  
+        return total
+      }
+
+    for (let apre of fatura.apresentacoes) {
+      const peca = pecas[apre.id];
+      let total = calcularTotalApresentacao(apre, peca)
       // créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
       if (peca.tipo === "comedia") 
